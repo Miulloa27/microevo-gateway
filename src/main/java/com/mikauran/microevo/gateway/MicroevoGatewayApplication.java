114f656800f71642106207a73125cd46f168d792
Miguel Lopez <miguel.lopez@nagarro.com>
2023-01-31 12:03:10 -0600

    Initial commit

---
+++ src/main/java/com/mikauran/microevo/gateway/MicroevoGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.mikauran.microevo.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroevoGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroevoGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mikauran/microevo/gateway/MicroevoGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikauran.microevo.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroevoGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
